78675c3367ba1b526b12c70e932f7631dd8f21b6
Giulio De Angelis <giuliodeangelis95@gmail.com>
2020-08-04 07:15:54 +0000

    Update

--- zexam-web/webapp/controller/Home.controller.js
+++ zexam-web/webapp/controller/Home.controller.js
@@ -9,10 +9,11 @@ sap.ui.define([
 	"../util/extensions/String"
 ], function (Controller, MessageToast, ODataModel, JSONModel) {
 	"use strict";
-
+	
 	return Controller.extend("zexam.zexam-web.controller.Home", {
 
 		onInit: function () {
+		    this._currentSeriesId = null;
 			this._bindModels();
 		},
 
@@ -21,21 +22,23 @@ sap.ui.define([
 		},
 
 		onSeriesPress: function (ev) {
-		    const that = this;
-		    const context = ev.getParameter("listItem").getBindingContext();
-		    const id = context.getObject().titoloSerie.replace(' ', "%20");
+			const that = this;
+			const context = ev.getParameter("listItem").getBindingContext();
+			const id = context.getObject().titoloSerie.replace(" ", "%20");
 			const path = "/Serie('" + id + "')/Puntate";
-		    
+			const producer = context.getObject().regista;
+
 			this.navTo("seriesDetail");
 
-            // bind the series form
+			// bind the series form
 			const form = this.byId("seriesDetailsForm");
 			form.setModel(context.getModel());
 			form.bindElement(context.getPath());
-		
-			// bind the table
+
+			// bind the episodes table
 			this.getView().getModel().read(path, {
 				success: function (data) {
+					for (const ep of data.results) ep.regista = producer;
 					that.byId("epsTable").setModel(new JSONModel(data));
 				},
 				error: function (err) {
@@ -47,29 +50,68 @@ sap.ui.define([
 
 		onAddButtonPress: function () {
 			this.navTo("seriesCreation");
+			this._currentSeriesId = null;
+		},
+		
+		onEditButtonPress: function () {
+			this._currentSeriesId = this.byId("seriesDetailsForm")
+			    .getBindingContext()
+			    .getObject()
+			    .titoloSerie;
+			    
+			this.navTo("seriesCreation");
 		},
 
-		onSaveSeriesButtonPress: function () {
-		    const that = this;
-			const model = this.getView().getModel();
-			const newSeriesModel = this.byId("seriesCreationForm").getModel();
+		onSaveSeriesButtonPress: function (ev) {
+// 			const that = this;
+// 			const model = this.getView().getModel();
+// 			const newSeriesModel = this.byId("seriesCreationForm").getModel();
+// 			const newEpsModel = this.byId("episodesCreationTable").getModel();
+// 			const seriesId = newSeriesModel.getData().titoloSerie;
+// 			const episodesPath = "/Serie('" + seriesId + "')/Puntate";
+
+			/* Logica di procedimento:
+			 * Quando currentSeriesId è null, allora l'operazione corrente è di create,
+			 * mentre se è definito sarà un'operazione di update.
+			 * L'operazione di create andrà a fare un batch di create sull'odata
+			 * utilizzando gli appositi metodi di batch, creando sia le serie
+			 * che gli episodi associati a quella serie.
+			 * L'operazione di update invece andrà prima di tutto a tenere un riferimento
+			 * degli episodi originali e di compararli alla lista degli episodi modificata
+			 * dall'utente. Quando un episodio esiste nella nuova lista ma non è presente
+			 * nella vecchia, allora si farà un'operazione di create, mentre se un episodio
+			 * esiste nella vecchia ma non nella nuova, allora si farà una delete.
+			 * Nella validazione bisogna vietare duplicati.
+		     */
+
+            if (this._currentSeriesId !== null)
+                this._updateSeries();
+            else this._createSeries();
 
 			// input validation
 			// TODO
 
-			// save series
-			model.create("/Serie", newSeriesModel.getData(), {
-				success: function () {
-					that._toast("seriesCreationSuccessMsg");
-				},
-				error: function (err) {
-					that._toast("seriesCreationErrorMsg");
-					console.log(err.message);
-				}
-			});
-
-			// save episodes
-			// TODO
+// 			model.create("/Serie", newSeriesModel.getData(), {
+// 				async: true,
+// 				success: function () {
+// 					that._toast("seriesCreationSuccessMsg");
+// 					// save episodes
+// 					model.create(episodesPath, newEpsModel.getData().episodi[0], {
+// 						async: true,
+// 						success: function () {
+// 						    that._toast("episodesCreationSuccessMsg")
+// 						},
+// 						error: function (err) {
+// 						    that._toast("episodesCreationErrorMsg");
+// 							console.log("Failed to create episodes");
+// 						}
+// 					});
+// 				},
+// 				error: function (err) {
+// 					that._toast("seriesCreationErrorMsg");
+// 					console.log(err.message);
+// 				}
+// 			});
 		},
 
 		onSaveEpButtonPress: function () {
@@ -79,13 +121,13 @@ sap.ui.define([
 			const newEpisodes = newEpisodesModel.getData();
 
 			// input validation
-			this._assertNotBlank(newEpisode.number); // TODO validate number
-			this._assertNotBlank(newEpisode.season);
-			this._assertNotBlank(newEpisode.title);
-			this._assertNotBlank(newEpisode.producer);
+			this._assertNotBlank(newEpisode.episodio); // TODO validate number
+			this._assertNotBlank(newEpisode.stagione);
+			this._assertNotBlank(newEpisode.titoloPuntata);
+			this._assertNotBlank(newEpisode.regista);
 
 			// push the episode to the table's model
-			newEpisodes.episodes.push(newEpisode);
+			newEpisodes.episodi.push(newEpisode);
 			newEpisodesModel.refresh();
 
 			// reset the form
@@ -98,28 +140,31 @@ sap.ui.define([
 			const newEps = newEpsModel.getData();
 
 			const indexes = table.getSelectedItems()
-				.map(it => it.getBindingContextPath().substringAfterLast('/'));
+				.map(it => it.getBindingContextPath().substringAfterLast("/"));
 
 			for (let i = newEps.episodes.length - 1; i >= 0; --i) {
-			    if (indexes.includes(i.toString()))
-			        newEps.episodes.splice(i, 1);
+				if (indexes.includes(i.toString()))
+					newEps.episodes.splice(i, 1);
 			}
-			
+
 			table.removeSelections(true);
 			newEpsModel.refresh();
-		},
-
-		onEditButtonPress: function () {
-			//  const seriesModel = this.byId("seriesCreationForm").getModel();
-			MessageToast.show("Not implemented");
 		},
 
 		onDeleteButtonPress: function () {
 			MessageToast.show("Not implemented");
 		},
-
-		onTestButtonPress: function () {
-			this.navTo("seriesDetail");
+		
+		_createSeries: function () {
+		    
+		},
+		
+		_updateSeries: function() {
+		    
+		},
+		
+		_deleteSeries: function() {
+		    
 		},
 
 		_bindModels: function () {
@@ -129,7 +174,7 @@ sap.ui.define([
 			model.read("/Serie", {
 				async: true,
 				urlParameters: {
-				    "$format": "json"
+					"$format": "json"
 				},
 				success: function (data) {
 					this.byId("seriesList").setModel(new JSONModel(data));
@@ -141,24 +186,24 @@ sap.ui.define([
 
 			// model for the series creation form
 			const newSeries = new JSONModel({
-				title: null,
-				genre: null,
-				date: null
+				titoloSerie: null,
+				genere: null,
+				anno: null
 			});
 			this.byId("seriesCreationForm").setModel(newSeries);
 
 			// model for the episode creation form
 			const newEpisode = new JSONModel({
-				number: null,
-				season: null,
-				title: null,
-				producer: null
+				episodio: null,
+				stagione: null,
+				titoloPuntata: null,
+				regista: null
 			});
 			this.byId("episodeCreationForm").setModel(newEpisode);
 
 			// model for the episodes creation table
 			const newEpisodes = new JSONModel({
-				episodes: []
+				episodi: []
 			});
 			this.byId("episodesCreationTable").setModel(newEpisodes);
 		},
